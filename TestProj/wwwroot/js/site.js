08d98f86810e0eaa44d247bf37d3083f2f6ac682
Alex Buhtev <alexbuhtev@gmail.com>
2026-03-03 15:52:50 +0200

    Added background and fixed minor issues

--- TestProj/wwwroot/js/site.js
+++ TestProj/wwwroot/js/site.js
@@ -1,4 +1,6 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿// Floating Background
+jQuery(document).ready(function () {
+    jQuery(".container-wrap").append(
+        "<ul class='circles'><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li></ul>"
+    );
+});
